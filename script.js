e794f57d2157ea48a6a5b6da3fb33941cc04b8b5
Sanjana M <sanjanasahana19@gmail.com>
2025-12-06 21:33:27 +0530

    JS Done

--- script.js
+++ script.js
@@ -16,12 +16,40 @@ function setupRecognition(recognition)
     recognition.continuous=true;
     recognition.interimResults=true;
     recognition.lan='en-US';
+    recognition.onresult=function(event)
+    {
+        //processResult()
+        const{
+            finalTranscript,interTranscript
+        }=processResult(event.results);
+        resultElement.innerHTML=finalTranscript+interTranscript
+    }
 }
 function processResult(results)
 {
-
+    let finalTranscript='';
+    let interTranscript='';
+    for(let i=0;i<results.length;i++)
+    {
+        let transcript=results[i][0].transcript;
+        transcript.replace("\n","<br>");
+        if(results[i].isFinal)
+        {
+            finalTranscript+=transcript;
+        }
+        else{
+            interTranscript+=transcript;
+        }
+    }
+    return
+    {
+        finalTranscript,interTranscript
+    }
 }
 function stopConverting()
 {
-
+    if(recognition)
+    {
+        recognition.stop();
+    }
 }
